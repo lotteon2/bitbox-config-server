2049f3f04dcec70755918f2b34ab750f57ceb715
최성훈 <33412452+realsuperman@users.noreply.github.com>
2023-10-06 12:45:43 +0900

    [FEAT] - 0.0.3 알파버전2 배포

---
+++ src/main/java/io/github/bitbox/bitbox/enums/PaymentType.java
@@ -0,0 +1,17 @@
+package io.github.bitbox.bitbox.enums;
+
+public enum PaymentType {
+    KAKAOPAY(100),
+    TOSSPAY(101),
+    ACCOUNT(102);
+
+    private final int value;
+
+    private PaymentType(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return this.value;
+    }
+}
