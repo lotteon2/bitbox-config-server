1e659b70c9d33589ffbeae23e2c771840d778cdb
최성훈 <33412452+realsuperman@users.noreply.github.com>
2023-10-13 16:47:30 +0900

    [FEAT] - 0.0.4 버전 업그레이드

--- src/main/java/io/github/bitbox/bitbox/config/JpaAuditingConfig.java
+++
@@ -1,9 +0,0 @@
-package io.github.bitbox.bitbox.config;
-
-
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-
-@EnableJpaAuditing
-public class JpaAuditingConfig {
-
-}
---
+++ src/main/java/io/github/bitbox/bitbox/jwt/JwtPayload.java
@@ -0,0 +1,14 @@
+package io.github.bitbox.bitbox.jwt;
+
+import io.github.bitbox.bitbox.enums.AuthorityType;
+import lombok.Builder;
+import lombok.Getter;
+
+@Builder
+@Getter
+public class JwtPayload {
+    private String memberId;
+    private Long classId;
+    private String memberNickname;
+    private AuthorityType memberAuthority;
+}
