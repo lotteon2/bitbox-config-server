7c433cbad8f17963c802bedb4b596a52d83ca3cb
최성훈 <33412452+realsuperman@users.noreply.github.com>
2023-10-17 20:06:33 +0900

    [FEAT] - 0.0.5 버전 업그레이드

--- src/main/java/io/github/bitbox/bitbox/dto/MemberTraineeResult.java
+++ src/main/java/io/github/bitbox/bitbox/dto/MemberTraineeResult.java
@@ -10,8 +10,4 @@ import java.util.List;
 public class MemberTraineeResult {
     private List<MemberValidDto> validMember;
     private List<MemberValidDto> invalidMember;
-
-    public static MemberTraineeResult convertLists(List<MemberValidDto> valid, List<MemberValidDto> invalid) {
-        return MemberTraineeResult.builder().validMember(valid).invalidMember(invalid).build();
-    }
 }
