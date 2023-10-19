751f45223f7657c34dad464aa1b05535bcc2fcd1
yun <indl1670@gmail.com>
2023-10-19 10:18:35 +0900

    [FIX] 교육생 유효성 검증 Result 수정

--- src/main/java/io/github/bitbox/bitbox/dto/MemberTraineeResult.java
+++ src/main/java/io/github/bitbox/bitbox/dto/MemberTraineeResult.java
@@ -1,12 +1,16 @@
 package io.github.bitbox.bitbox.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Builder
 @Getter
+@AllArgsConstructor
+@NoArgsConstructor
 public class MemberTraineeResult {
     private List<MemberValidDto> validMember;
     private List<MemberValidDto> invalidMember;
