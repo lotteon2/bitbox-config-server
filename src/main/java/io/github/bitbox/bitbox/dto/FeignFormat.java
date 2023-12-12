c9108dbc6a16521942f8382d02822bd46bb8edb9
최성훈 <33412452+realsuperman@users.noreply.github.com>
2023-12-12 17:57:20 +0900

    [FEAT] - 1.0.3 버전 업그레이드

--- src/main/java/io/github/bitbox/bitbox/dto/FeignFormat.java
+++ src/main/java/io/github/bitbox/bitbox/dto/FeignFormat.java
@@ -4,22 +4,23 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 /**
-     domain : all
-     detail : Feign 통신시 사용되는 Format Dto
-     method : Feign
-     comment : null column의 경우 값이 안보이는 속성 추가
+ * domain : all
+ * detail : Feign 통신시 사용되는 Format Dto
+ * method : Feign
+ * comment : null column의 경우 값이 안보이는 속성 추가
  */
 @AllArgsConstructor
+@NoArgsConstructor
 @Builder
 @Getter
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class FeignFormat<T> {
-
-    private final Integer code;
-    private final String message;
-    private final String detail;
-    private final String failure;
-    private final T data;
+    private Integer code;
+    private String message;
+    private String detail;
+    private String failure;
+    private T data;
 }
