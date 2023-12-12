eaf808c6393ab4524f2ee4d218cb7b6a05ebb59f
최성훈 <33412452+realsuperman@users.noreply.github.com>
2023-12-12 16:08:18 +0900

    [FEAT] - 1.0.1 버전 업그레이드

---
+++ src/main/java/io/github/bitbox/bitbox/util/KafkaTopicNameInfo.java
@@ -0,0 +1,33 @@
+package io.github.bitbox.bitbox.util;
+
+public class KafkaTopicNameInfo {
+    public static final String CREATE_ORDER = "create-order";
+    public static final String ADD_STOCK = "add-stock";
+    public static final String DELETE_CART = "delete-cart";
+    public static final String CREATE_AUCTION_ORDER = "create-auction-order";
+    public static final String UPDATE_PRODUCT_SALES_COUNT = "update-product-sales-count";
+    public static final String CANCEL_ORDER_POINT = "cancel-order-point";
+    public static final String CANCEL_ORDER_COUPON = "cancel-order-coupon";
+    public static final String CANCEL_ORDER_PAYMENT = "cancel-order-payment";
+    public static final String CANCEL_ORDER_STOCK = "cancel-order-stock";
+    public static final String ORDER_TOPIC_NAME = "reduce-point";
+    public static final String USE_COUPON = "use-coupon";
+    public static final String REDUCE_STOCK = "reduce-stock";
+    public static final String ROLLBACK_COUPON = "rollback_coupon";
+    public static final String CANCEL_KAKAOPAY = "cancel-kakaopay";
+    public static final String PAY_CANCEL_TOPIC = "cancel-order-payment";
+    public static final String UPDATE_CREDIT = "update-credit";
+    public static final String CREATE_SUBSCRIPTION = "create-subscription";
+    public static final String UPDATE_SELLER_TO_PRODUCT = "update-seller-to-search";
+    public static final String DELETE_SELLER_TO_PRODUCT = "delete-seller-to-product";
+    public static final String DELETE_SELLER_TO_REVIEW = "delete-seller-to-review";
+    public static final String CREATE_PRODUCT_TO_SEARCH = "create-product-to-search";
+    public static final String DELETE_PRODUCT_TO_SEARCH = "delete-product-to-search";
+    public static final String DELETE_PRODUCT_TO_WISH_CART = "delete-product-to-wish-cart";
+    public static final String DELETE_PRODUCT_TO_REVIEW = "delete-product-to-review";
+    public static final String UPDATE_PRODUCT_TO_SEARCH = "update-product-to-search";
+    public static final String UPDATE_PRODUCT_TO_REVIEW = "update-product-to-review";
+    public static final String UPDATE_IS_ACTIVATE = "update-is-activate";
+    public static final String UPDATE_CONSUMER_TO_REVIEW = "update-consumer-to-review";
+    public static final String UPDATE_REVIEW_POINT = "update-review-point";
+}
