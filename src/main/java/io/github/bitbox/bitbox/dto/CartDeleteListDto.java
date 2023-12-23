73f8c3ed692724570bfc5f5cf81d0008223f25d1
최성훈 <33412452+realsuperman@users.noreply.github.com>
2023-12-23 17:11:11 +0900

    [FEAT] - 1.2.4 버전 업그레이드

---
+++ src/main/java/io/github/bitbox/bitbox/dto/CartDeleteListDto.java
@@ -0,0 +1,16 @@
+package io.github.bitbox.bitbox.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class CartDeleteListDto {
+    private List<CartDeleteDto> cartDeleteDtoList;
+}
