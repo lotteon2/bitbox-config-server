95261cce4d19458f07b58024a79b5287c7ad8b66
최성훈 <33412452+realsuperman@users.noreply.github.com>
2023-12-23 14:33:23 +0900

    [FEAT] - 1.2.3 버전 업그레이드

--- src/main/java/io/github/bitbox/bitbox/dto/KakaoBatchDto.java
+++ src/main/java/io/github/bitbox/bitbox/dto/KakaoBatchDto.java
@@ -17,4 +17,14 @@ public class KakaoBatchDto implements SubscriptionBatchInterface {
     private Long quantity;
     private Long totalAmount;
     private Long taxFreeAmount;
+
+    public String settingSubscriptionDto() {
+        return "cid=" + cid
+                + "&sid=" + sid
+                + "&partner_order_id=" + partnerOrderId
+                + "&partner_user_id=" + partnerUserId
+                + "&quantity=" + quantity
+                + "&total_amount=" + totalAmount
+                + "&tax_free_amount=" + taxFreeAmount;
+    }
 }
--- src/main/java/io/github/bitbox/bitbox/dto/SubscriptionBatchInterface.java
+++ src/main/java/io/github/bitbox/bitbox/dto/SubscriptionBatchInterface.java
@@ -12,4 +12,5 @@ import com.fasterxml.jackson.annotation.JsonTypeInfo;
         @JsonSubTypes.Type(value = KakaoBatchDto.class, name = "kakaoBatch"),
 })
 public interface SubscriptionBatchInterface {
+    String settingSubscriptionDto();
 }
