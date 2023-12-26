45c0810356423cdcd341dc853e104406f42f99a5
woo yup Lee <zjadlspun1114@naver.com>
2023-12-26 09:30:02 +0900

    FEAT MemberInfoForNotificationDto 추가

---
+++ src/main/java/io/github/bitbox/bitbox/dto/MemberInfoForNotificationDto.java
@@ -0,0 +1,22 @@
+package io.github.bitbox.bitbox.dto;
+
+import io.github.bitbox.bitbox.enums.NotificationTypeEnum;
+import io.github.bitbox.bitbox.enums.RecipientTypeEnum;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Getter
+public class MemberInfoForNotificationDto {
+
+    private Long recipientId;
+    private RecipientTypeEnum recipientType;
+    private NotificationTypeEnum notificationType;
+    private LocalDateTime createdAt;
+}
