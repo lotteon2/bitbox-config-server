8a905d9fa6be505d88fca77a01dd1839519093c7
woo yup Lee <zjadlspun1114@naver.com>
2023-12-26 17:49:43 +0900

    FIX

--- src/main/java/io/github/bitbox/bitbox/util/KafkaTopicNameInfo.java
+++ src/main/java/io/github/bitbox/bitbox/util/KafkaTopicNameInfo.java
@@ -35,4 +35,5 @@ public class KafkaTopicNameInfo {
     public static final String ISSUE_REGULAR_PAYMENTS_COUPON = "issue-regular-payments-coupon";
     public static final String PAYMENT_SUBSCRIPTION = "payment-subscription";
     public static final String SEND_NOTIFICATION = "send-notification";
+    public static final String SEND_ERROR_NOTIFICATION = "send-error-notification";
 }
