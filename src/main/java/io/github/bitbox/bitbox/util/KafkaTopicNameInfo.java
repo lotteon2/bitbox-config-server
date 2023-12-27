fc3899328ed786c1961dd67f91b4a1edc80a7879
최성훈 <33412452+realsuperman@users.noreply.github.com>
2023-12-27 20:42:22 +0900

    [FEAT] - 1.3.0 버전 업그레이드

--- src/main/java/io/github/bitbox/bitbox/util/KafkaTopicNameInfo.java
+++ src/main/java/io/github/bitbox/bitbox/util/KafkaTopicNameInfo.java
@@ -36,5 +36,5 @@ public class KafkaTopicNameInfo {
     public static final String PAYMENT_SUBSCRIPTION = "payment-subscription";
     public static final String SEND_NOTIFICATION = "send-notification";
     public static final String SEND_ERROR_NOTIFICATION = "send-error-notification";
-    public static final String SETTLEMENT_FILE_UPDATE = "settlement-file-upload";
+    public static final String SETTLEMENT_FILE_UPLOAD = "settlement-file-upload";
 }
