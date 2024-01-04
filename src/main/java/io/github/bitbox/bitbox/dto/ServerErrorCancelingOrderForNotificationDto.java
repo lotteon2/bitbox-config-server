f5887d487c2a49606437d93122c08b746c339521
최성훈 <33412452+realsuperman@users.noreply.github.com>
2024-01-04 11:19:35 +0900

    [FEAT] - 1.3.3 버전 업그레이드

--- src/main/java/io/github/bitbox/bitbox/dto/ServerErrorCancelingOrderForNotificationDto.java
+++ src/main/java/io/github/bitbox/bitbox/dto/ServerErrorCancelingOrderForNotificationDto.java
@@ -1,7 +1,6 @@
 package io.github.bitbox.bitbox.dto;
 
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
