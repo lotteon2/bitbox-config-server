ff0bd8fbf462148c5621c05e3b6dc38c06be653a
woo yup Lee <zjadlspun1114@naver.com>
2024-01-04 11:18:37 +0900

    FEAT

---
+++ src/main/java/io/github/bitbox/bitbox/dto/ImpAuthInfoForUpdateDto.java
@@ -0,0 +1,16 @@
+package io.github.bitbox.bitbox.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Getter
+public class ImpAuthInfoForUpdateDto {
+
+    private String name;
+    private String phoneNumber;
+}
