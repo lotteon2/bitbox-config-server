13bfb8be41c7be3aeecb054908d35d9d7feb0b12
최성훈 <33412452+realsuperman@users.noreply.github.com>
2024-01-04 17:50:16 +0900

    [FEAT] - 1.3.4 버전 업그레이드

---
+++ src/main/java/io/github/bitbox/bitbox/dto/ProductImageInfoDto.java
@@ -0,0 +1,15 @@
+package io.github.bitbox.bitbox.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Getter
+public class ProductImageInfoDto {
+    private String productId;
+    private String productThumbnailImageUrl;
+}
