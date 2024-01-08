34c97ce9d2d77c5f715f9d0e0a5244afb165c574
woo yup Lee <zjadlspun1114@naver.com>
2024-01-08 11:29:39 +0900

    FEAT

---
+++ src/main/java/io/github/bitbox/bitbox/dto/GenderTypeEnum.java
@@ -0,0 +1,13 @@
+package io.github.bitbox.bitbox.dto;
+
+public enum GenderTypeEnum {
+
+    MEN("남"),
+    WOMEN("여");
+
+    private String value;
+
+    GenderTypeEnum(String value) {
+        this.value = value;
+    }
+}
